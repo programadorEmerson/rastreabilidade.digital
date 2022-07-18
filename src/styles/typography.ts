39140d0244c21b102f2f089197828b6af62f67cd
Emerson Saturnino <programador.emerson@gmail.com>
2022-07-18 16:14:26 -0300

    Configurações iniciais com Material UI

---
+++ src/styles/typography.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const typography = createTheme({
+  typography: {
+    fontFamily: 'Poppins, Roboto, sans-serif',
+  },
+});
+
+export default typography;
