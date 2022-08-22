7cf7934fc68f55410ec9a7ba0d60a622d4ff94cc
Emerson Saturnino <programador.emerson@gmail.com>
2022-08-22 10:35:09 -0300

    :wrench: chore: Adicionando arquivos de configuração do husk1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import type { NextPage } from 'next';
 
@@ -15,6 +15,7 @@ import { TOKEN_PREFIX, TOKEN_REGISTER_PREFIX } from '@utils/tokensPrefix';
 import { destroyCookie, parseCookies } from 'nookies';
 
 const Home: NextPage = () => {
+  const [teste, setTeste] = useState(0);
   const { setTokenUser } = useAuthContext();
   const theme = useTheme();
   const { type } = useDeviceType();
